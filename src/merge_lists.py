0ab3f0f3347f7401725115bc978ac59e1cccbae3
Beatriz <biabiasouza17@gmail.com>
2026-05-05 23:20:46 -0300

    merge lists

--- src/merge_lists.py
+++ src/merge_lists.py
@@ -2,4 +2,27 @@ from src.my_node import MyNode
 
 
 def merge_lists(lista1: MyNode, lista2: MyNode) -> MyNode:
-    raise NotImplementedError
+    if lista1 is None:
+        return lista2
+    if lista2 is None:
+        return lista1
+    if lista1.value < lista2.value:
+        head = lista1
+        lista1 = lista1.next
+    else:
+        head = lista2
+        lista2 = lista2.next
+    current = head
+    while lista1 is not None and lista2 is not None:
+        if lista1.value < lista2.value:
+            current.next = lista1
+            lista1 = lista1.next
+        else:
+            current.next = lista2
+            lista2 = lista2.next
+        current = current.next
+    if lista1 is not None:
+        current.next = lista1
+    else:
+        current.next = lista2
+    return head
