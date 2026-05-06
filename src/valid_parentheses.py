1c67a9bfe1309018be3de7d1a7f13d4d90e62bf2
Beatriz <biabiasouza17@gmail.com>
2026-05-05 22:56:19 -0300

    valid parentheses

--- src/valid_parentheses.py
+++ src/valid_parentheses.py
@@ -1,5 +1,19 @@
-# from src.my_stack import MyStack
+from src.my_stack import MyStack
 
 
 def is_valid_parentheses(string: str) -> bool:
-    raise NotImplementedError
+    stack = MyStack()
+    for char in string:
+        if char in "({[":
+            stack.push(char)
+        else:
+            if stack.is_empty():
+                return False
+            top = stack.pop()
+            if char == ")" and top != "(":
+                return False
+            if char == "}" and top != "{":
+                return False
+            if char == "]" and top != "[":
+                return False
+    return stack.is_empty()
