58835cd463aa3df8436d24af5690bf5a52109237
lucas-allb <albuquerque.trabalho5625@gmail.com>
2026-05-10 15:42:22 -0300

    is_valid_parentheses-concluido

--- src/valid_parentheses.py
+++ src/valid_parentheses.py
@@ -1,5 +1,22 @@
-# from src.my_stack import MyStack
+from src.my_stack import MyStack
 
 
 def is_valid_parentheses(string: str) -> bool:
-    raise NotImplementedError
+       stack = MyStack()
+       par = {")": "(",
+              "]": "[",
+              "}": "{"}
+       
+       for char in string:
+           if char in "([{":
+               stack.push(char)
+
+           elif char in ")]}":
+                if stack.is_empy():
+                    return False
+                
+                remover = stack.pop()
+                if remover != par[char]:
+                    return False
+        
+       return stack.is_empyt()
