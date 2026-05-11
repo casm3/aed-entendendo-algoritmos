47afa044237c1b8c67504fb245ccf2f1ac147aa3
Reniée <reniee_@hotmail.com>
2026-05-11 17:17:31 -0300

    resolução questão 3

--- src/valid_parentheses.py
+++ src/valid_parentheses.py
@@ -1,5 +1,33 @@
-# from src.my_stack import MyStack
+from src.my_stack import MyStack
 
 
 def is_valid_parentheses(string: str) -> bool:
-    raise NotImplementedError
+    stack = MyStack()
+
+    pares = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
+
+    for caractere in string:
+
+        # Se for abertura, empilha
+        if caractere in "([{":
+            stack.push(caractere)
+
+        # Se for fechamento
+        elif caractere in ")]}":
+
+            # Se a pilha estiver vazia, inválido
+            if stack.is_empty():
+                return False
+
+            topo = stack.pop()
+
+            # Verifica se corresponde ao par correto
+            if topo != pares[caractere]:
+                return False
+
+    # No final, a pilha deve estar vazia
+    return stack.is_empty()
