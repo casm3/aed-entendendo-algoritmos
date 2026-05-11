5fbb69f5ee4de58789d23073f5398121fd426c37
lucas-allb <albuquerque.trabalho5625@gmail.com>
2026-05-10 21:56:14 -0300

    kth_to_last-concluido

--- src/kth_to_last.py
+++ src/kth_to_last.py
@@ -2,4 +2,19 @@ from src.my_node import MyNode
 
 
 def kth_to_last(head: MyNode, k: int) -> int:
-    raise NotImplementedError
+    if not head or k <= 0:
+        return -1
+    
+    fast = head
+    slow = head
+
+    for m in range(k):
+        if fast is None:
+            return -1
+        fast = fast.next
+
+    while fast:
+        slow = slow.next
+        fast = fast.next
+
+    return slow.value
