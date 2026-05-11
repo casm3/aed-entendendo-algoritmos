29730496dece00a08b9ebd3bf7896b8219aa4db5
Reniée <reniee_@hotmail.com>
2026-05-11 18:16:15 -0300

    Resposta das questões 6 e 7

--- src/kth_to_last.py
+++ src/kth_to_last.py
@@ -2,4 +2,22 @@ from src.my_node import MyNode
 
 
 def kth_to_last(head: MyNode, k: int) -> int:
-    raise NotImplementedError
+    lento = head
+    rapido = head
+
+    # Avança o ponteiro rápido k posições
+    for _ in range(k):
+        if rapido is None:
+            return -1
+        rapido = rapido.next
+
+    # Move os dois ponteiros até o rápido chegar ao final
+    while rapido is not None:
+        lento = lento.next
+        rapido = rapido.next
+
+    # Se a lista estiver vazia
+    if lento is None:
+        return -1
+
+    return lento.value
