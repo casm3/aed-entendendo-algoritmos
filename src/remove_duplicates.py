cf1ab88a314bbc455c434d427d471d022a1bb66f
lucas-allb <albuquerque.trabalho5625@gmail.com>
2026-05-10 21:42:04 -0300

    remove_duplicates-concluido

--- src/remove_duplicates.py
+++ src/remove_duplicates.py
@@ -2,4 +2,19 @@ from src.my_node import MyNode
 
 
 def remove_duplicates(head: MyNode) -> MyNode:
-    raise NotImplementedError
+    if not head:
+        return None
+    
+    atual = head
+    while atual:
+        runner = atual
+
+        while runner.next:
+            if runner.next.value == atual.value:
+                runner.next = runner.next.next
+            else:
+                runner = runner.next
+
+        atual = atual.next
+
+    return head 
