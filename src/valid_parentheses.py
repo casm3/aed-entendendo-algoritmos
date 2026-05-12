8a243a082e3f267cef8ae3b3ea35217220006bae
Adriel <adrielrodrigues156@gmail.com>
2026-05-12 08:51:49 -0300

    feat: Implementar a função `is_valid_parentheses` para verificar a validade de parênteses

--- src/valid_parentheses.py
+++ src/valid_parentheses.py
@@ -1,5 +1,17 @@
-# from src.my_stack import MyStack
+from src.my_stack import MyStack
 
 
 def is_valid_parentheses(string: str) -> bool:
-    raise NotImplementedError
+    pilhas = MyStack()
+    pares = {')': '(', '}': '{', ']': '['}
+
+    for caractere in string:
+        if caractere in "({[":
+            pilhas.push(caractere)
+        elif caractere in ")}]":
+            if pilhas.is_empty():
+                return False
+            topo = pilhas.pop()
+            if pares[caractere] != topo:
+                return False
+    return pilhas.is_empty()
