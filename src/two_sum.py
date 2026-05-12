888e9ba0dac3c94d7b9b32f3746fcc4b724de9ba
Paulo Patricio <paulocdn99@gmail.com>
2026-05-12 16:29:15 -0300

    Implementando função two_sum

--- src/two_sum.py
+++ src/two_sum.py
@@ -1,5 +1,11 @@
 from src.my_array import MyArray
 
 
-def two_sum(array: MyArray, target: int) -> tuple[int, int]:
-    raise NotImplementedError
+from src.my_array import MyArray
+
+def two_sum(array: MyArray, alvo: int) -> tuple[int, int]:
+    for i in range(len(array)):
+        for j in range(i + 1, len(array)):
+            if array.get(i) + array.get(j) == alvo:
+                return (i, j)
+    return (-1, -1)
