2cf785bcc1627cb5a7be78758dfbf791838aff12
Adriel <adrielrodrigues156@gmail.com>
2026-05-12 08:51:10 -0300

    feat: Implementar a função `remove_duplicates` para remover duplicatas em uma lista encadeada

--- src/remove_duplicates.py
+++ src/remove_duplicates.py
@@ -2,4 +2,16 @@ from src.my_node import MyNode
 
 
 def remove_duplicates(head: MyNode) -> MyNode:
-    raise NotImplementedError
+    if not head:
+        return None
+    atual = head
+    while atual:
+        proximo = atual
+
+        while proximo.next:
+            if atual.value == proximo.next.value:
+                proximo.next = proximo.next.next
+            else:
+                proximo = proximo.next
+        atual = atual.next
+    return head
