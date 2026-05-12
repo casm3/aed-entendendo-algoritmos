b9e7710e34c04a74b95ebf5d9e7ceb8c0f714fdd
PauloQueirozz <paulosamuelqb@gmail.com>
2026-05-11 21:00:26 -0300

    update lista 3

--- src/quick_sort.py
+++ src/quick_sort.py
@@ -1,5 +1,31 @@
 from src.my_array import MyArray
 
 
-def quick_sort(array: MyArray) -> MyArray:
-    raise NotImplementedError
+def quick_sort(arr: MyArray) -> MyArray:
+    _quick_sort(arr, 0, len(arr) - 1)
+    return arr
+
+
+def _quick_sort(arr: MyArray, low: int, high: int) -> None:
+    if low < high:
+        pivot_index = _partition(arr, low, high)
+        _quick_sort(arr, low, pivot_index - 1)
+        _quick_sort(arr, pivot_index + 1, high)
+
+
+def _partition(arr: MyArray, low: int, high: int) -> int:
+    pivot = arr.get(high)
+    i = low - 1
+
+    for j in range(low, high):
+        if arr.get(j) <= pivot:
+            i += 1
+            temp = arr.get(i)
+            arr.set(i, arr.get(j))
+            arr.set(j, temp)
+
+    temp = arr.get(i + 1)
+    arr.set(i + 1, arr.get(high))
+    arr.set(high, temp)
+
+    return i + 1
