095b5cb46cc3d84978f3d5e5c97e5b0b623daee7
Paulo Patricio <paulocdn99@gmail.com>
2026-05-12 17:03:40 -0300

    Implementa merge_sort para MyArray usando recursão

--- src/merge_sort.py
+++ src/merge_sort.py
@@ -1,5 +1,46 @@
 from src.my_array import MyArray
 
+def merge_sort(arr: MyArray) -> MyArray:
+    # chama a função recursiva com índices
+    return _merge_sort(arr, 0, len(arr) - 1)
 
-def merge_sort(array: MyArray) -> MyArray:
-    raise NotImplementedError
+
+def _merge_sort(arr: MyArray, inicio: int, fim: int) -> MyArray:
+    if inicio >= fim:
+        return arr
+
+    meio = (inicio + fim) // 2
+    _merge_sort(arr, inicio, meio)
+    _merge_sort(arr, meio + 1, fim)
+    _merge(arr, inicio, meio, fim)
+
+    return arr
+
+
+def _merge(arr: MyArray, inicio: int, meio: int, fim: int):
+
+    esquerda = [arr.get(i) for i in range(inicio, meio + 1)]
+    direita = [arr.get(i) for i in range(meio + 1, fim + 1)]
+
+    i = j = 0
+    k = inicio
+
+    while i < len(esquerda) and j < len(direita):
+        if esquerda[i] <= direita[j]:
+            arr.set(k, esquerda[i])
+            i += 1
+        else:
+            arr.set(k, direita[j])
+            j += 1
+        k += 1
+
+
+    while i < len(esquerda):
+        arr.set(k, esquerda[i])
+        i += 1
+        k += 1
+
+    while j < len(direita):
+        arr.set(k, direita[j])
+        j += 1
+        k += 1
