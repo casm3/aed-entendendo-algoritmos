1714f5c4092b20e00668619dc1800652a8753446
João Barreto <Anothermailb@gmail.com>
2026-05-12 18:39:34 -0300

    Update two_sum.py

--- src/two_sum.py
+++ src/two_sum.py
@@ -2,4 +2,8 @@ from src.my_array import MyArray
 
 
 def two_sum(array: MyArray, target: int) -> tuple[int, int]:
-    raise NotImplementedError
+    for i in range(len(array)):
+        for j in range(len(array)):
+            if array[i] + array[j] == target and i != j:
+                return (i, j)
+    return (-1, -1)
