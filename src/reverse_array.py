13d7fceb20b2a0031266109771a656ff57b5f66e
felipefeiitosa <felipe.feiitosa08@gmail.com>
2026-05-12 21:30:30 -0300

    Exercicios 01,02,03 - Resolvidos

--- src/reverse_array.py
+++ src/reverse_array.py
@@ -2,4 +2,15 @@ from src.my_array import MyArray
 
 
 def reverse_array(array: MyArray) -> MyArray:
-    raise NotImplementedError
+    i = 0
+    j = len(array) - 1
+
+    while i < j:
+        temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+
+        i += 1
+        j -= 1
+
+    return array
--- src/valid_parentheses.py
+++ src/valid_parentheses.py
@@ -1,5 +1,20 @@
-# from src.my_stack import MyStack
+from src.my_stack import MyStack
 
 
 def is_valid_parentheses(string: str) -> bool:
-    raise NotImplementedError
+    test_stack = MyStack()
+    mapping = {")": "(", "]": "[", "}": "{"}
+
+    for char in string:
+        if char in mapping:
+            if test_stack.is_empty():
+                return False
+
+            top = test_stack.pop()
+
+            if mapping[char] != top:
+                return False
+        else:
+            test_stack.push(char)
+
+    return test_stack.is_empty()
