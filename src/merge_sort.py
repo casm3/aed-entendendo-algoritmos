c8a9322046a67848c2d718ba21f8dec865087976
Carlos Souza <cadu.dev99@gmail.com>
2026-05-12 22:11:48 -0300

    merge_sort e quick_sort

--- src/merge_sort.py
+++ src/merge_sort.py
@@ -2,4 +2,38 @@ from src.my_array import MyArray
 
 
 def merge_sort(array: MyArray) -> MyArray:
-    raise NotImplementedError
+    if len(array) <= 1:
+        return array
+
+    mid = len(array) // 2
+    left = MyArray()
+    for i in range(mid):
+        left.append(array[i])
+    right = MyArray()
+    for i in range(mid, len(array)):
+        right.append(array[i])
+
+    return _merge(merge_sort(left), merge_sort(right))
+
+def _merge(left: MyArray, right: MyArray) -> MyArray:
+    merged = MyArray()
+    i = 0
+    j = 0
+
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+
+    while i < len(left):
+        merged.append(left[i])
+        i += 1
+
+    while j < len(right):
+        merged.append(right[j])
+        j += 1
+
+    return merged
