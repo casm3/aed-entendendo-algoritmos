3a7c54e539619299b7fa07a5b0ba84a6207c68b5
Lucas Pontes <lucas.pfsousa@upe.br>
2026-05-12 22:13:06 -0300

    feat: implement various algorithms including cycle detection, kth to last, merge, sorting, and linked list operations

--- src/quick_sort.py
+++ src/quick_sort.py
@@ -1,5 +1,28 @@
 from src.my_array import MyArray
 
 
-def quick_sort(array: MyArray) -> MyArray:
-    raise NotImplementedError
+def quick_sort(array: MyArray, inicio=0, fim=None) -> MyArray:
+    if fim is None:
+        fim = len(array) - 1
+    if inicio < fim:
+        indice_pivo = particionar(array, inicio, fim)
+
+        quick_sort(array, inicio, indice_pivo - 1)
+
+        quick_sort(array, indice_pivo + 1, fim)
+
+    return array
+
+
+def particionar(array, inicio, fim):
+    pivo = array[fim]
+    i = inicio - 1
+
+    for j in range(inicio, fim):
+        if array[j] <= pivo:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+
+    array[i + 1], array[fim] = array[fim], array[i + 1]
+
+    return i + 1
--- src/reverse_array.py
+++ src/reverse_array.py
@@ -2,4 +2,15 @@ from src.my_array import MyArray
 
 
 def reverse_array(array: MyArray) -> MyArray:
-    raise NotImplementedError
+    inicio = 0
+    fim = len(array) - 1
+
+    while inicio < fim:
+        temporaria = array[inicio]
+        array[inicio] = array[fim]
+
+        array[fim] = temporaria
+
+        inicio += 1
+        fim -= 1
+    return array
